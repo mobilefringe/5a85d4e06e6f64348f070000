3189b6ef8eb2aaa90b56028a3ec0d8d5de1e7ee2
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-15 15:35:25 -0500

    Milton Mall 2.0 1.1  /routes.js

--- routes.js
+++ routes.js
@@ -14,7 +14,7 @@ define([], function () {
                     meta: {
                         pageName: 'Community Space',
                     },
-                    name: 'contact'
+                    name: 'community'
                 }
             ]
         },
