971fd3ca2389be9797e93eaaa96a47e4a22b9be9
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-03-02 13:29:33 -0500

    Milton Mall 2.0 1.1  /kids_club.vue

---
+++ kids_club.vue
@@ -0,0 +1,15 @@
+<template>
+    <div> <!-- without an outer container div this component template will not render -->
+        <loading-spinner v-if="!dataLoaded"></loading-spinner>
+        <transition name="fade">
+            <inside-header-component></inside-header-component>
+        </transition>
+        <transition name="fade">
+            <div v-if="dataLoaded" v-cloak class="main_container margin_30">
+                <div class="row">
+                
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
