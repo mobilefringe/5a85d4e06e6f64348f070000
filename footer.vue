c7c391552c4452e42ce2ba1e73cccf2ca94a1a62
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2018-03-26 11:27:43 -0400

    Milton Mall 2.0 1.1  /footer.vue

--- footer.vue
+++ footer.vue
@@ -2,7 +2,7 @@
     <footer>
         <section class="footer_logo">
             <div>
-                <img alt="Property Logo" class="site_logo" src="//codecloud.cdn.speedyrails.net/sites/5a69eb436e6f644297010000/image/png/1517945546000/Group 4@2x.png"/>    
+                <img alt="Property Logo" class="site_logo" src="//codecloud.cdn.speedyrails.net/sites/5a85d4e06e6f64348f070000/image/png/1522078168000/ProudlyManaged_V_dark_web.png"/>    
             </div>
             <div class="footer_social">
                 <div class="social_icons">
@@ -53,34 +53,5 @@
 </template>
 
 <script>
-    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue!search-component"], function (Vue, Vuex, moment, tz, VueMoment, SearchComponent) {
-        return Vue.component("footer-component", {
-            template: template, // the variable template will be injected,
-            data: function data() {
-                return {
-                    suggestionAttribute: 'name',
-                    search: '' 
-                }
-            },
-            props:['social_media'],
-            computed: {
-                ...Vuex.mapGetters([
-                    'property',
-                    'processedStores'
-                ]),
-                copyright_year() {
-                    return moment().year();
-                }
-            },
-            methods: {
-                onOptionSelect(option) {
-                    console.log('Selected option:', option);
-                    this.$nextTick(function() {
-                        this.search = "";
-                    });
-                    this.$router.push("/stores/" + option.slug);
-                }
-            }
-        });
-    });
+var _extends=Object.assign||function(e){for(var t=1;t<arguments.length;t++){var n=arguments[t];for(var o in n)Object.prototype.hasOwnProperty.call(n,o)&&(e[o]=n[o])}return e};define(["Vue","vuex","moment","moment-timezone","vue-moment","vue!search-component"],function(e,t,n){return e.component("footer-component",{template:template,data:function(){return{suggestionAttribute:"name",search:""}},props:["social_media"],computed:_extends({},t.mapGetters(["property","processedStores"]),{copyright_year:function(){return n().year()}}),methods:{onOptionSelect:function(e){console.log("Selected option:",e),this.$nextTick(function(){this.search=""}),this.$router.push("/stores/"+e.slug)}}})});
 </script>
