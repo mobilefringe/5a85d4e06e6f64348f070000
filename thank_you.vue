f21c2901a14924c8dc020af160157d321d947d0d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-03-28 16:12:02 -0400

    Milton Mall 2.0 1.1  /thank_you.vue

---
+++ thank_you.vue
@@ -0,0 +1,58 @@
+<template>
+    <div> <!-- without an outer container div this component template will not render -->
+        <loading-spinner v-if="!dataLoaded"></loading-spinner>
+        <transition name="fade">
+            <inside-header-component></inside-header-component>
+        </transition>
+        <transition name="fade">
+            <div v-if="dataLoaded" v-cloak class="main_container margin_30">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="margin_30">
+                            <p>Your subscription has been confirmed. You've been added to our list and will hear from us soon.</p>    
+                        </div>
+                        <router-link to="/">
+                            <div class="fill_btn">Back to Site</div>
+                        </router-link>
+                    </div>
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "vuex", "jquery", "axios", "vue-meta"], function(Vue, Vuex, $, axios, Meta) {
+        Vue.use(Meta);
+        return Vue.component("kids-club-component", {
+            template: template, // the variable template will be injected
+            props:['locale'],
+            data: function() {
+                return {
+                    dataLoaded: false,
+                }
+            },
+            created() {
+                this.loadData().then(response => {
+                    this.dataLoaded = true;
+                });
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property'
+                ]),
+            },
+            methods: {
+                loadData: async function() {
+                    try {
+                        // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
+                        let results = await Promise.all([this.$store.dispatch("getData", "repos")]);
+                        return results;
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                }
+            }
+        });
+    });
+</script>
