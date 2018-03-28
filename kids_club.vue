1b7c775b8676bf626c95b8a1e88f351484af6dd4
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-03-28 15:15:15 -0400

    Milton Mall 2.0 1.1  /kids_club.vue

--- kids_club.vue
+++ kids_club.vue
@@ -16,36 +16,36 @@
                 		<form id="subForm" class="form-horizontal js-cm-form" action="https://www.createsend.com/t/subscribeerror?description=" method="post" data-id="92D4C54F0FEC16E5ADC2B1904DE9ED1AB518424692B1918726CC1BDEC952DE8B96532C4DD58B1C389252E53F3C68E748D5F4932FCBD3A00FA5F4EE555698A95C">
                             <div class="form-group ">
     							<div class="col-sm-4 col-xs-12">
-                                    <label for="fielddhkjhh">Child&#x27;s Name</label><br />
+                                    <label for="fielddhkjhh">Child&#x27;s Name *</label><br />
                                     <input id="fielddhkjhh" class="form-control" name="cm-f-dhkjhh" type="text" />
                                 </div>
                                 <div class="col-sm-4 col-xs-12">
-                                    <label for="fieldEmail">Parent/Guardian's Email</label><br />
+                                    <label for="fieldEmail">Parent/Guardian's Email *</label><br />
                                     <input id="fieldEmail" class="form-control js-cm-email-input" name="cm-ukihdd-ukihdd" type="email" required />
                                 </div>
                                 <div class="col-sm-4 col-xs-12">
-                                    <label for="fielddhkjkt">Postal Code</label><br />
+                                    <label for="fielddhkjkt">Postal Code *</label><br />
                                     <input id="fielddhkjkt" class="form-control" name="cm-f-dhkjkt" type="text" />
                                 </div>
                             </div>
+                            <!--<div class="form-inline">-->
+                            <!--    <label class="checkbox">-->
+                            <!--        <input name="newsletter" required type="checkbox" >-->
+                            <!--        I agree to receive emails from Milton Mall regarding Kids Club.-->
+                            <!--    </label>-->
+                            <!--</div>-->
                             <div class="form-inline">
                                 <label class="checkbox" for="listiljuii">
-                                    <input id="listiljuii" name="cm-ol-iljuii" type="checkbox" />
-                                    Yes! Sign me up for Milton Mall's Newsletter
+                                    <input id="listiljuii" name="cm-ol-iljuii" type="checkbox" required />
+                                    I agree to receive emails form Milton Mall and Milton Monkeys’ Kids’ Club
                                 </label>
                             </div>
-                            <div class="form-inline">
-                                <label class="checkbox">
-                                    <input name="newsletter" required type="checkbox" >
-                                    I agree to receive emails from Milton Mall.
-                                </label>
-                            </div>
-                            <div class="form-inline">
-                                <label class="checkbox">
-                                    <input name="privacy_policy" required type="checkbox">
-                                    I agree to the <a href="/pages/milton-privacy-policy" target="_blank">Privacy Policy</a>.
-                                </label>
-                            </div>
+                            <!--<div class="form-inline">-->
+                            <!--    <label class="checkbox">-->
+                            <!--        <input name="privacy_policy" required type="checkbox">-->
+                            <!--        I agree to the <a href="/pages/milton-privacy-policy" target="_blank">Privacy Policy</a>.-->
+                            <!--    </label>-->
+                            <!--</div>-->
                             <div class="form-group ">
     							<div class="col-xs-12">
                                     <button class="fill_btn js-cm-submit-button" type="submit">Submit</button> 
