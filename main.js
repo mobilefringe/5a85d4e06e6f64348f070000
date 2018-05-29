7176d0c5fbd7ed8c0ea93c6bb89d6f68f164719b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-29 13:18:05 -0400

    Milton Mall 2.0 1.2  /main.js

--- main.js
+++ main.js
@@ -119,7 +119,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
                 try {
                     await this.$store.dispatch('initializeApi', { site: "milton", version: "v4" });
                     await Promise.all([this.$store.dispatch("getData", "property")]);
-                    this.property.mm_host = this.property.mm_host.replace("http:", "");
+                    // this.property.mm_host = this.property.mm_host.replace("http:", "");
                     let results = await Promise.all([this.$store.dispatch("INITIALIZE_LOCALE"), this.$store.dispatch("getData", "hours"), this.$store.dispatch("getData", "stores")]);
                     await Promise.all([this.$store.dispatch("LOAD_META_DATA")]);
                 } catch (e) {
