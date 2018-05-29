4a29af8860025523b2f537a676bd00aebeb2160f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-29 12:59:28 -0400

    Milton Mall 2.0 1.2  /main.js

--- main.js
+++ main.js
@@ -88,6 +88,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
         },
         created: function ()  {
             this.loadData().then(response => {
+                this.property.mm_host = this.property.mm_host.replace("http:", "");
                 this.$store.dispatch("setDataLoaded", true);      
             });
         },
