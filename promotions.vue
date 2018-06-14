cbe29216d8c688f1829b89e7ab42faaf8f9a316f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-14 15:50:02 -0400

    Milton Mall 2.0 1.2  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -70,7 +70,7 @@
                             if (_.includes(value.image_url, 'missing')) {
                                 value.image_url = "https://placehold.it/400x400";
                             }
-                            value.name = _.truncate(value.name, { 'length': 100, 'separator': ' ' });
+                            value.name = _.truncate(value.name, { 'length': 50, 'separator': ' ' });
                             showPromos.push(value);
                         }
                     });
