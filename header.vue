72f2dc6c5813ff4d8a4a7cd665b2ba13046b3636
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-14 17:59:09 -0400

    Milton Mall 2.0 1.3  /header.vue

---
+++ header.vue
@@ -0,0 +1,218 @@
+<template>
+    <header>
+        <section id="header" class="header main_container">
+            <div class="row logo_container">
+                <div class="col-md-2">
+                    <div class="site_logo center-block">
+                        <a href="/">
+                            <img :alt="'property.name'+ Logo" src="//codecloud.cdn.speedyrails.net/sites/5b22de6b6e6f640dbb2c0000/image/png/1519152520000/logo.png">
+                        </a>
+                    </div>
+                    <div @click="show_menu = !show_menu" :class="{ open: show_menu }" id="menu-icon">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+                <div class="col-md-10">
+                    <div class="header_social_container hidden-sm hidden-xs">
+                        <div class="header_social">
+                            <div class="social_icons">
+                                <span v-for="item in social_media">
+                                    <a :href="item.url" target="_blank">
+                                        <i :class="item.iconClass" aria-hidden="true"></i>
+                                    </a>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="header_newsletter">
+                            <div class="header_newsletter_container">
+                                <label for="header_newsletter" class="hidden_now">Enter Email Address</label>
+                                <input id="header_newsletter" class="newsletter_input" type="text" placeholder="Join Our Newsletter!" v-model="newsletter_email"/>
+                                <router-link :to="'/newsletter?email='+ newsletter_email">
+                                    <label for="header_newsletter_submit"  class="hidden_now">Submit</label>
+                                    <input id="header_newsletter_submit" class="newsletter_btn" type="submit" value="Subscribe"/>
+                                </router-link>
+                            </div>
+                        </div>
+                    </div>
+                    <transition name="custom-classes-transition" @enter="killTransition" @leave="killTransition" enter-active-class="animated slideInRight" leave-active-class="animated slideOutRight">
+                        <nav id="primary_nav" v-if="show_menu" v-on:keyup.esc="show_menu = false">
+                            <div class="todays_hours" v-if="todays_hours">
+                                <span v-if="!todays_hours.is_closed">
+                                    Open Today - {{todays_hours.open_time | moment("h:mma", timezone)}} to {{todays_hours.close_time | moment("h:mma", timezone)}}
+                                </span>
+                                <span v-if="todays_hours.is_closed">
+                                    Closed Today
+                                </span>
+                            </div>
+                            <ul>
+                                <li id="dropDown1" @click="toggleSubMenu('dropDown1')" class="menu_item">Store Directory
+                                    <ul :class="{ show_submenu: showSubMenu1 }">
+                                        <router-link tag="li" to="/stores" class="submenu_item" exact>
+                                            <a>Store Directory</a>
+                                        </router-link>
+                                        <router-link tag="li" to="/pages/milton-office-directory" class="submenu_item" exact>
+                                            <a>Office Directory</a>
+                                        </router-link>
+                                        <router-link tag="li" to="/map" class="submenu_item" exact>
+                                            <a>Centre Map</a>
+                                        </router-link>
+                                    </ul>
+                                </li>
+                                <router-link tag="li" to="/events" class="menu_item" exact>
+                                    Events
+                                </router-link>
+                                <router-link tag="li" to="/promotions" class="menu_item" exact>
+                                    Promotions
+                                </router-link>
+                                <li id="dropDown2" @click="toggleSubMenu('dropDown2')" class="menu_item">Leasing
+                                    <ul :class="{ show_submenu: showSubMenu2 }">
+                                        <router-link tag="li" to="/pages/milton-leasing" class="submenu_item" exact>
+                                            <a>Leasing</a>
+                                        </router-link>
+                                        <router-link tag="li" to="/photos" class="submenu_item" exact>
+                                            <a>Photos</a>
+                                        </router-link>
+                                    </ul>
+                                </li>
+                                <li id="dropDown3" @click="toggleSubMenu('dropDown3')" class="menu_item">Contact Us
+                                    <ul :class="{ show_submenu: showSubMenu3 }">
+                                        <router-link tag="li" to="/contact-us" class="submenu_item" exact>
+                                            <a>Contact Us</a>
+                                        </router-link>
+                                        <router-link tag="li" to="/directions" class="submenu_item" exact>
+                                            <a>Find Us</a>
+                                        </router-link>
+                                        <router-link tag="li" to="/pages/milton-about-us" class="submenu_item" exact>
+                                            <a>About Us</a>
+                                        </router-link>
+                                        <router-link tag="li" to="/jobs" class="submenu_item" exact>
+                                            <a>Jobs</a>
+                                        </router-link>
+                                        <router-link tag="li" to="/hours" class="submenu_item" exact>
+                                            <a>Hours</a>
+                                        </router-link>
+                                        <router-link tag="li" to="/community" class="submenu_item" exact>
+                                            <a>Community</a>
+                                        </router-link>
+                                    </ul>
+                                </li>
+                            </ul>
+                            <div class="mobile_social_icons">
+                                <span v-for="item in social_media">
+                                    <a :href="item.url" target="_blank">
+                                        <i :class="item.iconClass" aria-hidden="true"></i>
+                                    </a>
+                                </span>
+                            </div>
+                        </nav>
+                    </transition>
+                </div>
+            </div>
+        </section>
+    </header>
+</template>
+
+<script>
+    define(["Vue", "vuex", "vue_router", "routes", "vue!today_hours.vue"], function (Vue, Vuex, VueRouter, appRoutes, TodayHoursComponent) {
+        return Vue.component("header-component", {
+            template: template, // the variable template will be injected,
+            data: function () {
+                return {
+                    active: false, 
+                    newsletter_email: "",
+                    isOpen: false,
+                    windowWidth: 0,
+                    show_menu: true,
+                    showSubMenu1: false,
+                    showSubMenu2: false,
+                    showSubMenu3: false,
+                    stopInitTrans: true,
+                }
+            },
+            props:['social_media'],
+            watch: {
+                $route: function() {
+                    if (this.windowWidth <= 768) {
+                        this.show_menu = false;
+                    }  
+                },
+                windowWidth: function() {
+                    if (this.windowWidth <= 768) {
+                        this.show_menu = false;
+                    } else {
+                        this.show_menu = true;
+                    }
+                },
+                show_menu: function() {
+                    if(this.show_menu == true && this.windowWidth <= 768){
+                        document.body.classList.add("no-scroll");
+                    } else if (this.show_menu == false) {
+                        document.body.classList.remove("no-scroll");
+                    }
+                }
+            },
+            mounted() {
+                this.$nextTick(function() {
+                    window.addEventListener('resize', this.getWindowWidth);
+                    //Init
+                    this.getWindowWidth();
+                });
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone',
+                    'hours',
+                    'getTodayHours',
+                ]),
+                locale: {
+                    get () {
+                        return this.$store.state.locale
+                    },
+                    set (value) {
+                        this.$store.commit('SET_LOCALE', { lang: value })
+                    }
+                },
+                todays_hours() {
+                    return this.getTodayHours;
+                }
+            },
+            methods: {
+                changeLocale: function(val) {
+                    // this will update the data store, which in turn will trigger the watcher to update the locale in the system
+                    this.locale = val; 
+                },
+                getWindowWidth(event) {
+                    this.windowWidth = window.innerWidth;
+                },
+                toggleSubMenu(id) {
+                    this.showSubMenu1 = false;
+                    this.showSubMenu2 = false;
+                    this.showSubMenu3 = false;
+                    
+                    if(id == "dropDown1"){
+                        this.showSubMenu1 = true   
+                    } else if (id == "dropDown2"){
+                        this.showSubMenu2 = true 
+                    } else if (id == "dropDown3"){
+                        this.showSubMenu3 = true 
+                    }
+                    
+                },
+                //this function kills the transition on the initial load
+                killTransition (el, done) {
+                   if(this.stopInitTrans) {
+                    this.stopInitTrans = false;
+                    done();
+                   }
+                }
+            },
+            beforeDestroy: function() {
+                window.removeEventListener('resize', this.getWindowWidth);
+            }
+        });
+    });
+</script>
