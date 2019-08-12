5056e7cbd9d9a85f20ee72a3b5d741a3a630c634
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-08-12 11:47:46 -0400

    Milton Mall 2.0 1.3  /contest.vue

--- contest.vue
+++ contest.vue
@@ -2,7 +2,7 @@
     <div> <!-- without an outer container div this component template will not render -->
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
-            <inside-header-component></inside-header-component>
+            <!--<inside-header-component></inside-header-component>-->
         </transition>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak class="main_container margin_30">
