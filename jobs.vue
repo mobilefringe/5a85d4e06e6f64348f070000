0f08a5b0a28df3aecc89cec8533406091aef46c4
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-08-13 11:03:38 -0400

    Milton Mall 2.0 1.3  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -46,7 +46,29 @@
                 ...Vuex.mapGetters([
                     'property',
                     'processedJobs'
-                ])
+                ]),
+                jobs() {
+                    var vm = this;
+                    var job_list = this.processedJobs;
+                    var temp_job = [];
+                    _.forEach(job_list, function(value, key) {
+                        today = moment().tz(vm.timezone);
+                        webDate = moment(value.show_on_web_date).tz(vm.timezone);
+                        if (today >= webDate) {
+                            value.description_short = _.truncate(value.description, { 'length': 150 });
+                            
+                            if (value.store  && _.includes(value.store.store_front_url_abs, 'missing')) {
+                                value.store.no_store_logo = true;
+                            } else if (!value.store) {
+                                value.store = {};
+                                value.store.store_front_url_abs = vm.property.default_logo_url;
+                            }
+                            temp_job.push(value);
+                        }
+                    });
+
+                    return temp_job;
+                },
             },
             methods: {
                 loadData: async function () {
